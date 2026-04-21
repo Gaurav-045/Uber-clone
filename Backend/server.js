0a291ebfcaf52c2ea49a6ee4d86c712d7222f55f
Gaurav-045 <gauravkakade739@gmail.com>
2026-04-21 14:23:34 +0530

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const app = require('./src/app');
+
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
