17e7d864d613c773f106bda8fb0b218eb0bd2978
Gaurav-045 <gauravkakade739@gmail.com>
2026-05-16 22:41:13 +0530

    using ref and some logic added smooth open and close for pickUp and destination location

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { use, useRef, useState } from 'react'
 
 const Home = () => {
+
+  const [pickup,setPickup] = useState('');
+  const [destination,setDestination] = useState('');
+
+  const panelRef = useRef();
+  const panelRef2 = useRef();
+
+  const submitHandler = () =>{
+    e.preventDefault();
+  }
+
+  const panelFun1 = ()=>{
+    panelRef.current.style.height = '70%';
+    panelRef2.current.classList.remove('hidden');
+  }
+  const panelFun2 = () =>{
+    panelRef.current.style.height = '0%';
+    panelRef2.current.classList.add('hidden');
+  }
+
+
   return (
     <div className='m-0 p-0 relative h-screen'>
 
@@ -12,14 +33,50 @@ const Home = () => {
       </div>
 
       <div className=' flex flex-col justify-end absolute top-0 w-full h-screen'>
-        <div className='h-[30%] p-5 bg-white'>
-          <form >
-            <input className='bg-[#eee] p-3 w-full  my-3' type="text" placeholder='enter pick up point' />
-            <input className='bg-[#eee] p-3 w-full  my-3' type="text" placeholder='enter destination' />
+        <div className='h-[30%] p-5 bg-white relative'>
+
+        <h4 className='mb-2 font-semibold'>Find my trip</h4>
+        <button 
+        onClick={()=>{
+          
+          panelFun2();
+        }}
+        ref={panelRef2}
+        className='absolute right-5 top-4 bg-[#abf8bd] px-2 py-1 rounded-2xl hidden'>close</button>
+
+          <form onSubmit={(e) =>{
+            submitHandler(e);
+          }}>
+
+            <input 
+            
+            onClick={()=>{
+              
+              panelFun1();
+            }}
+            value={pickup}
+            onChange={(e) =>{
+              setPickup(e.target.value)
+            }}
+            className='bg-[#eee] p-3 w-full  my-3' type="text" placeholder='enter pick up point' />
+
+            <input
+            
+            onClick={()=>{
+              
+              panelFun1();
+            }} 
+            value={destination}
+            onChange={(e) =>{
+              setDestination(e.target.value)
+            }}
+            className='bg-[#eee] p-3 w-full  my-3' type="text" placeholder='enter destination' />
           </form>
         </div>
-        <div className='h-[70%] p-4 bg-red-300 w-full hidden '>
-              content
+        <div 
+        ref={panelRef}
+        className='bg-red-300 w-full '>
+            
         </div>
       </div>
 
