c2a59dd341d6642aa12db1670c26d0a4ca49e1c7
Gaurav-045 <gauravkakade739@gmail.com>
2026-05-30 18:49:24 +0530

    added socket io at backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const http = require('http');
+const {initiateSocket} = require('./socket');
+
 const port = process.env.PORT || 3000;
 
 const app = require('./src/app');
 
 
 const server = http.createServer(app);
+initiateSocket(server);
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ Backend/socket.js
@@ -0,0 +1,36 @@
+const {Server} = require('socket.io');
+const userModel = require('./src/models/user.model');
+const captainModel = require('./src/models/captain.model');
+
+let io;
+
+async function initiateSocket(server){
+    io = new Server(server,{
+        cors:{
+            origin:"*",
+            methods:["GET","POST"],
+            credentials:true
+        }
+    }
+    )
+
+    io.on("connection",(socket) =>{
+        console.log("New user connected : ", socket.id);
+
+        socket.on('join', async(data) =>{
+            const {userId,userType} = data;
+
+            if (userType === 'user'){
+                await userModel.findByIdAndUpdate(userId,{socketId:socket.id});
+            }else if(userType === 'captain'){
+                await captainModel.findByIdAndUpdate(userId,{socketId:socket.id});
+            }
+        })
+
+        socket.on('disconnect',()=>{
+            console.log("user disconnected : ", socket.id);
+        })
+    })
+}
+
+module.exports.initiateSocket = initiateSocket;
